b9b4c9321a54f719c1a7312cfaa93e8c8d3ca065
mFStarBoy483 <149820969+mFStarBoy483@users.noreply.github.com>
2024-02-19 22:22:21 +0530

    Add files via upload

---
+++ redux/store.js
@@ -0,0 +1,11 @@
+// src/redux/store.js
+import { configureStore } from "@reduxjs/toolkit";
+import {productApi} from "./api/productsApi";
+
+export const store = configureStore({
+  reducer: {
+    [productApi.reducerPath]: productApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productApi.middleware),
+});
